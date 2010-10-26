7bfcf1e59ec3683d0adda578abb247857ec09f2d
Wander Nauta <info@wandernauta.nl>
2010-10-26 13:27:46 +0200

    Misc changes

--- culprit.py
+++ culprit.py
@@ -9,6 +9,9 @@ import curses.wrapper
 import time
 import csv
 import StringIO
+import subprocess
+
+smbcache = {}
 
 class Requester:
 	raddress = "192.168.0.1"
@@ -23,6 +26,7 @@ class Requester:
 	def fetch(self):
 		url = '%s:%s@%s/goform/updateIptAccount' % (self.username, self.password, self.raddress)
 		return urllib.urlopen('http://' + url).read()
+		
 
 def main():
 	r = Requester()
@@ -42,6 +46,7 @@ def parse(text):
 	for row in reader:
 		record = {}
 		record['ip'] = row[0]
+		record['hostname'] = resolveip(row[0])
 		record['uprate'] = float(row[1])
 		record['downrate'] = float(row[2])
 		# Sent packages (3) is skipped
@@ -49,21 +54,49 @@ def parse(text):
 		# Received packages (5) is skipped
 		record['recvmb'] = float(row[6])
 		records.append(record)
-	return reversed(sorted(records, key=lambda k: k['uprate']))
+	
+	reversedrecords = reversed(sorted(records, key=lambda k: k['uprate']))
+	sumup = sum(record['uprate'] for record in records)
+	sumdown = sum(record['downrate'] for record in records)
+	return (reversedrecords, sumup, sumdown)
 
-def window(mainwin, text):
+def resolveip(ip):
+	global smbcache
+	
+	if smbcache.has_key(ip):
+		return smbcache[ip]
+		
+	status(curses.sb, "Resolving {}".format(ip))
+	try:
+		host = subprocess.check_output(["smbutil", "status", ip]).split('\n')[1].split(':')[1].strip()
+		smbcache[ip] = host
+	except Exception:
+		smbcache[ip] = ip
+		return ip
+	return host
+
+def window(mainwin, text, r):
 	mainwin.clear()
 	mainwin.move(0,0)
-	maxlines = mainwin.getmaxyx()[0] - 1
+	maxlines = mainwin.getmaxyx()[0] - 3
 	
-	mainwin.addstr("{:<15} {:>8} {:>8} {:>15} {:>15}\n".format("IP/HOST", "UPLOAD", "DOWNLOAD", "SENT MB", "RECV MB"))
+	(records, sumup, sumdown) = parse(text)
+	
+	mainwin.addstr("Culprit.py 0.1 listening to {0.raddress} - Total up: {1} Total down: {2} \n".format(r, sumup, sumdown))
+	mainwin.addstr("\n")
+	mainwin.addstr("{:<15} {:>8} {:>8} {:>15} {:>15}\n".format("IP/HOST", "UPLOAD", "DOWNLOAD", "SENT MB", "RECV MB"), curses.A_BOLD)
 	
 	i = 0
-	for record in parse(text):
+	for record in records:
 		if i == maxlines:
 			break
+		if record['hostname'] == record['ip']:
+			attr = curses.A_DIM
+		else:
+			attr = curses.A_NORMAL
+			
 		try:
-			mainwin.addstr("{0[ip]:<15} {0[uprate]:>8.2f} {0[downrate]:>8.2f} {0[sendmb]:>15.2f} {0[recvmb]:>15.2f}\n".format(record))
+			mainwin.addstr("{0[hostname]:<15} {0[uprate]:>8.2f} {0[downrate]:>8.2f} {0[sendmb]:>15.2f} {0[recvmb]:>15.2f}\n".format(record), attr)
 		except curses.error:
 			pass
 		i += 1
@@ -72,15 +105,15 @@ def window(mainwin, text):
 
 def cmain(stdscr, r):
 	(maxy, maxx) = stdscr.getmaxyx()
-	sb = curses.newwin(1, maxx, maxy-1, 0)	
+	curses.sb = curses.newwin(1, maxx, maxy-1, 0)	
 	mainwin = curses.newwin(maxy-1, maxx, 0, 0)
 	
 	while True:
-		status(sb, "Fetching...")
+		status(curses.sb, "Fetching...")
 		dat = r.fetch()
-		status(sb, "Parsing...")
-		window(mainwin, dat)
-		status(sb, "Fetched %d records" % len(dat.split('\n')))
+		status(curses.sb, "Parsing...")
+		window(mainwin, dat, r)
+		status(curses.sb, "Fetched %d records" % len(dat.split('\n')))
 		time.sleep(5)
 
 if __name__ == '__main__':
